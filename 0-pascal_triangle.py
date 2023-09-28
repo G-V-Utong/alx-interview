316dc6675474980bc17567619ac8fabe0c8d7b0c
root <root@G-V-Utong>
2023-09-28 22:42:53 +0100

    pascal's triangle

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,19 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    # Initializing Pascal's triangle with the first row
+    triangle = [[1]]
+
+    while len(triangle) < n:
+        # Generating the next row based on the previous row
+        prev_row = triangle[-1]
+        next_row = [1]
+
+        for i in range(1, len(prev_row)):
+            next_row.append(prev_row[i - 1] + prev_row[i])
+
+        next_row.append(1)
+        triangle.append(next_row)
+
+    return triangle
